0b71ccdcedb518f8410d4333fe88bb598751af55
MassimoParisi <massimop98@outlook.it>
2023-04-13 09:57:09 +0200

    feat: Google auth, custom error messages on sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import { type GetServerSideProps, type NextPage } from "next";
 import { getSession, signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession();
+  const { error } = useRouter().query;
 
   return (
     <>
@@ -18,76 +20,50 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        {status === "authenticated" && (
-          <div className="flex flex-col items-center justify-center gap-8">
-            Welcome back {session.user.name}! <GroupView />
-            <button
-              className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-              onClick={() => void signOut()}
-            >
-              Sign out
-            </button>
-          </div>
-        )}
-        {status === "unauthenticated" && <LoginView />}
+      <main className="flex min-h-screen flex-col items-center justify-center gap-8">
+        <h2 className="text-3xl">
+          Sign in to create a Secret Santa group or join an existing one.
+        </h2>
+        <div className="flex flex-row items-center justify-center gap-4">
+          <button
+            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={() => void signIn("discord")}
+          >
+            Sign in with Discord
+          </button>
+          <button
+            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={() => void signIn("google")}
+          >
+            Sign in with Google
+          </button>
+        </div>
+        {error && <SignInError error={error as keyof typeof errors} />}
       </main>
     </>
   );
 };
 
-export const GroupView = () => {
-  const { data: groups, isLoading, isError } = api.groups.getAll.useQuery();
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <p>Error</p>;
-  }
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-8">
-      <h2 className="text-2xl">Your groups</h2>
-      <ul className="flex flex-col gap-4">
-        {groups.map((group) => (
-          <li key={group.id}>
-            <Link
-              href={`/groups/${group.id}`}
-              className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-            >
-              {group.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Link
-        className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-        href="/groups/create"
-      >
-        Create a group
-      </Link>
-    </div>
-  );
-};
-
-const LoginView = () => {
-  return (
-    <div className="flex flex-col items-center justify-center gap-8">
-      <p>Sign in to create a Secret Santa group or join an existing one.</p>
-
-      <button
-        className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-        onClick={() => void signIn("discord", { callbackUrl: "/dashboard" })}
-      >
-        Sign in with Discord
-      </button>
-    </div>
-  );
-};
-
 export default Home;
+
+const errors = {
+  Signin: "Try signing with a different account.",
+  OAuthSignin: "Try signing with a different account.",
+  OAuthCallback: "Try signing with a different account.",
+  OAuthCreateAccount: "Try signing with a different account.",
+  EmailCreateAccount: "Try signing with a different account.",
+  Callback: "Try signing with a different account.",
+  OAuthAccountNotLinked:
+    "To confirm your identity, sign in with the same account you used originally.",
+  EmailSignin: "Check your email address.",
+  CredentialsSignin:
+    "Sign in failed. Check the details you provided are correct.",
+  default: "Unable to sign in.",
+};
+const SignInError: React.FC<{ error: keyof typeof errors }> = ({ error }) => {
+  const errorMessage = error && (errors[error] ?? errors.default);
+  return <div className="text-red-500">{errorMessage}</div>;
+};
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
